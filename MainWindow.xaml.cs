64b5b7e31c49f952a2e9e30d17271ab296b8e561
liwenjian <tdj.lwj@163.com>
2020-12-02 23:29:47 +0800

    DispatcherTimer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Shutdown
 {
@@ -20,9 +23,48 @@ namespace Shutdown
     /// </summary>
     public partial class MainWindow : Window
     {
+        int time = 60000;
+        bool up = true;
+        DispatcherTimer Timer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+            Timer.Start();
+            /*
+            while (up)
+            {
+                Thread.Sleep(1000);
+                timeSpan = DateTime.Now - beginTime;
+                if (timeSpan.TotalSeconds > time)
+                    up = false;
+            }
+            Process.Start("c:/windows/system32/shutdown.exe", "-s");
+            */
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Start()
+        {
+            DateTime beginTime = DateTime.Now;
+            TimeSpan timeSpan;
+            while (up)
+            {
+                //Thread.Sleep(1000);
+                timeSpan = DateTime.Now - beginTime;
+                if (timeSpan.TotalSeconds > time)
+                    up = false;
+            }
+            Process.Start("c:/windows/system32/shutdown.exe", "-s");
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+          
         }
     }
 }
